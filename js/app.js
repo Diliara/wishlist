a47f00abc9bba8a662b17f9d496547f5469bd49e
diliara <contact@diliaranasirova.com>
2015-01-11 12:03:37 -0800

    products.js module, setup dependency

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+/* top-level module attached via ng-app */
+
 (function () {
-    var app = angular.module('wishlist', []);
+    var app = angular.module('wishlist', ['wishlist-products']);
 
     /* Controller - where we define our app's behaviour by defining functions and values */
     app.controller('WishlistController', function () {
@@ -45,56 +47,7 @@
         }
     ];
 
-    app.directive('productDescription', function () {
 
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-description.html'
-        };
-    });
-
-    app.directive('productTitle', function() {
-
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-title.html'
-        };
-
-    });
-
-    app.directive('productLink', function() {
-
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-link.html'
-        };
-
-    });
-
-    app.directive('productPrice', function() {
-
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-price.html'
-        };
-
-    });
-    app.directive('productImage', function() {
-
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-image.html'
-        };
-
-    });
-    app.directive('productCheckBtn', function() {
-
-        return {
-            restrict: 'E',
-            templateUrl: 'includes/product-check-btn.html'
-        };
-
-    });
 
 
 })();
---
+++ js/products.js
@@ -0,0 +1,55 @@
+/* all the functionality for products */
+
+(function () {
+    var app = angular.module('wishlist-products', [])
+
+
+    app.directive('productDescription', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-description.html'
+        };
+    });
+    app.directive('productTitle', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-title.html'
+        };
+
+    });
+    app.directive('productLink', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-link.html'
+        };
+
+    });
+    app.directive('productPrice', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-price.html'
+        };
+
+    });
+    app.directive('productImage', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-image.html'
+        };
+
+    });
+    app.directive('productCheckBtn', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'includes/product-check-btn.html'
+        };
+
+    });
+
+
+})();
